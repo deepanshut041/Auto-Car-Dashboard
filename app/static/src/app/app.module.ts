ec3288284dc5382062c0e80e1849673e6e0c2795
deepanshut041 <deepanshut041@gmail.com>
2017-11-28 18:23:22 +0530

    added adm maps now working on changing marker and making it clickable

--- app/static/src/app/app.module.ts
+++ app/static/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from "./app-routing.module";
+import { AgmCoreModule } from '@agm/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from "./login/login.component";
@@ -12,7 +13,9 @@ import { DashboardComponent } from "./dashboard/dashboard.component";
     AppComponent, LoginComponent, DashboardComponent
   ],
   imports: [
-    BrowserModule, AppRoutingModule
+    BrowserModule, AppRoutingModule,AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAcXrL1ag0b0f0CW5rQ_dGyh5ZhsMNTUc0'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- app/static/src/app/dashboard/dashboard.component.ts
+++ app/static/src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 
+
 @Component({
   selector: "app-dashboard",
   templateUrl: "./dashboard.component.html",
@@ -7,12 +8,42 @@ import { Component, OnInit } from "@angular/core";
 })
 
 export class DashboardComponent implements OnInit {
-  
-  constructor() { 
+
+  lat: number = 51.678418;
+  lng: number = 7.809007;
+
+  markers =[
+    {
+      lat : 51.278418,
+      lng : 7.609007,
+    },
+    {
+      lat : 51.58418,
+      lng : 7.409007,
+    },
+    {
+      lat : 51.478418,
+      lng : 7.609007,
+    },
+    {
+      lat : 51.38418,
+      lng : 7.409007,
+    }
+  ]
+
+  constructor() {
 
   }
 
   ngOnInit() {
+    console.log(this.markers)
+    setInterval(()=>{
+      this.markers.map((marker)=>{
+        marker.lat +=.01
+        marker.lng +=.01
+      })
+      console.log(this.markers)
+    },3000)
 
   }
 }
