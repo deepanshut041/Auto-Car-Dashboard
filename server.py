a047e2a6d32623c22c5ca8e2b48a44d3a22ebe1f
deepanshut041 <deepanshut041@gmail.com>
2017-11-29 00:02:20 +0530

    Added serial monitor on seprate thread

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 import os
 from flask import Flask, render_template ,request ,send_from_directory
+import threading
+import serial
 # -------------------Database Setup----------------------------------------
 #
 # from database_setup import Base,Restaurant,MenuItem
@@ -18,6 +20,20 @@ from flask import Flask, render_template ,request ,send_from_directory
 app=Flask(__name__)
 
 root = os.path.join(os.path.dirname(os.path.abspath(__file__)), "app", "static", "dist")
+ser = serial.Serial('/dev/ttyACM0', 9600)
+#This code is for taking lat and lon from serial monitor 
+#
+def set_interval(func, sec):
+    def func_wrapper():
+        set_interval(func, sec) 
+        func()  
+    t = threading.Timer(sec, func_wrapper)
+    t.start()
+    return t
+
+def func():
+	read_serial = ser.readLine()
+	print(read_serial)
 
 @app.route('/<path:path>', methods=['GET'])
 def static_proxy(path):
@@ -25,11 +41,13 @@ def static_proxy(path):
 
 @app.route('/', methods=['GET'])
 def redirect_to_index():
-    return send_from_directory(root, 'index.html')
+	set_interval(func, 1)
+	return send_from_directory(root, 'index.html')
 
 @app.route('/login', methods=['GET'])
 def redirect_to_login():
-    return send_from_directory(root, 'index.html')
+	set_interval(func, 1)
+	return send_from_directory(root, 'index.html')
 
 @app.route('/dashboard', methods=['GET'])
 def redirect_to_dashboard():
